75002e5eefa469cdb541d6190539fca717a6886a
guanyi.gl <guanyi.gl@alibaba-inc.com>
2020-12-18 20:42:55 +0800

    uint32

--- core/graphBolt/KickStarterEngine.h
+++ core/graphBolt/KickStarterEngine.h
@@ -108,6 +108,7 @@ public:
     uintV parent;
     T value;
     uint32_t level;
+    uint32_t dummy;
     DependencyData() : level(MAX_LEVEL), value(), parent(MAX_PARENT) {}
 
     DependencyData(uint32_t _level, T _value, uint32_t _parent)
